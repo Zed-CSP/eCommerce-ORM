95b92d8ae78d7192a7130840d144b891a77161d3
Christopher Peret <cperet89@gmail.com>
2023-07-12 15:19:24 -0700

    catagory model

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer
+      allowNull: false, 
+      primaryKey: true, // set as primary key
+      autoIncrement: true // uses auto increment for id's, UUID implimentation would be better irl
+    },
+    category_name: {
+      type: DataTypes.STRING, // string
+      allowNull: false 
+    }
   },
   {
     sequelize,
