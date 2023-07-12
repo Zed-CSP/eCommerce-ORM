93be144010a604ced6e9509a871448785e8638dc
Christopher Peret <cperet89@gmail.com>
2023-07-12 15:26:31 -0700

    ProductTag def

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER, // integer
+      allowNull: false,
+      primaryKey: true, // set as primary key
+      autoIncrement: true // uses auto increment for id's, UUID implimentation would be better irl
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // integer
+      references: {
+        model: 'product', // references the product model
+        key: 'id' // uses the id column
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // integer
+      references: {
+        model: 'tag', // references the tag model
+        key: 'id' // uses the id column
+      }
+    }
   },
   {
     sequelize,
